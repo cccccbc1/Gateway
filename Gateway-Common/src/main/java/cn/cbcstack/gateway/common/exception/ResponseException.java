e0c1bd7d242c73685a122c4359ebe5965a4b3d95
cccccbc <1137398949@qq.com>
2025-09-08 21:56:29 +0800

    common模块添加通用返回和异常

---
+++ Gateway-Common/src/main/java/cn/cbcstack/gateway/common/exception/ResponseException.java
@@ -0,0 +1,22 @@
+package cn.cbcstack.gateway.common.exception;
+
+
+import cn.cbcstack.gateway.common.enums.ResponseCode;
+
+import java.io.Serial;
+
+public class ResponseException extends GatewayException {
+
+    @Serial
+    private static final long serialVersionUID = 707018357827678269L;
+
+    public ResponseException(ResponseCode code) {
+        super(code.getMessage(), code);
+    }
+
+    public ResponseException(Throwable cause, ResponseCode code) {
+        super(code.getMessage(), cause, code);
+        this.code = code;
+    }
+
+}
