bbd0d8cb1631e5f39e8ad6eb816a1aa9d07375a2
cccccbc <1137398949@qq.com>
2025-09-09 16:18:02 +0800

    nacos注册中心配置

---
+++ Gateway-Config/src/main/java/cn/cbcstack/gateway/config/test/config/NettyConfig.java
@@ -0,0 +1,18 @@
+package cn.cbcstack.gateway.config.test.config;
+
+
+import lombok.Data;
+
+/**
+ * Netty配置项
+ */
+@Data
+public class NettyConfig {
+
+    private int eventLoopGroupBossNum = 1;
+
+    private int eventLoopGroupWorkerNum = Runtime.getRuntime().availableProcessors() * 2;
+
+    private int maxContentLength = 64 * 1024 * 1024; // 64MB
+
+}
---
+++ Gateway-Config/src/main/java/cn/cbcstack/gateway/config/test/config/RegisterCenterConfig.java
@@ -0,0 +1,15 @@
+package cn.cbcstack.gateway.config.test.config;
+
+import cn.cbcstack.gateway.common.constant.RegisterCenterConstant;
+import cn.cbcstack.gateway.common.enums.RegisterCenterEnum;
+
+
+public class RegisterCenterConfig {
+
+    private RegisterCenterEnum type = RegisterCenterConstant.REGISTER_CENTER_DEFAULT_IMPL; // 注册中心实现
+
+    private String address = RegisterCenterConstant.REGISTER_CENTER_DEFAULT_ADDRESS; // 注册中心地址
+
+    private NacosConfig nacos = new NacosConfig(); // 注册中心nacos配置
+
+}
